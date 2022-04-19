7407e55bb7a65c9b433bc41c089de2cdb3d5f689
Anthony <istafa.a.marshall@gmail.com>
2022-04-19 19:17:25 -0400

    commit tree a1804d512a94202b772b283db0d61a9d4dc41135

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Follow along with the examples here
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
